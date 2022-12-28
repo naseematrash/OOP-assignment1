2ba57c43b2f7a86889b84cdb762c736f34b36e78
naseem atrash <naseematrash12@gmail.com>
2022-12-28 15:14:48 +0200

    test for UndoableStringBuilderTest

--- OOP.Assignment1/src/test/java/ConcreteMemberTest.java
+++ OOP.Assignment1/src/test/java/ConcreteMemberTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ConcreteMemberTest {
+public class ConcreteMemberTest {
 
     @Test
     void addme() {
