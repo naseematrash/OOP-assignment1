4f8210f69f8636da735febf0d271a577e088b789
naseem atrash <naseematrash12@gmail.com>
2022-12-29 18:39:13 +0200

    trying a Tests.java

--- OOP.Assignment1/src/test/java/Tests.java
+++ OOP.Assignment1/src/test/java/Tests.java
@@ -14,14 +14,70 @@ public class Tests {
         GroupAdmin admin =new GroupAdmin();
         ConcreteMember member1 = new ConcreteMember(admin);
         ConcreteMember member2 = new ConcreteMember(admin);
-        ConcreteMember member3 = new ConcreteMember(admin);
 
-       // logger.info(()->JvmUtilities.objectFootprint(s1));
 
-        //logger.info(()->JvmUtilities.objectFootprint(s1,s2));
+        System.out.println("jvm info: ");
+        logger.info(() -> JvmUtilities.jvmInfo());
 
-        //logger.info(()->JvmUtilities.objectTotalSize(s1));
+        System.out.println("objectfootprint: ");
+        logger.info(()->JvmUtilities.objectFootprint(admin,member1,member2));
+        System.out.println("totalsize: ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin,member1,member2));
 
-        //logger.info(() -> JvmUtilities.jvmInfo());
+
+        System.out.println("objectfootprintfor admin.append ");
+        admin.append("hello!");
+        logger.info(()->JvmUtilities.objectFootprint(admin));
+        System.out.println("totalsize for admin.append ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin));
+
+
+
+        System.out.println("objectfootprintfor admin.unregister ");
+        admin.unregister(member2);
+        logger.info(()->JvmUtilities.objectFootprint(admin));
+        System.out.println("totalsize for admin.unregister ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin));
+
+
+        System.out.println("objectfootprintfor admin.insert ");
+        admin.insert(2,"norway");
+        logger.info(()->JvmUtilities.objectFootprint(admin));
+        System.out.println("totalsize for admin.insert ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin));
+
+
+
+        System.out.println("objectfootprintfor admin.delete ");
+        admin.delete(1,3);
+        logger.info(()->JvmUtilities.objectFootprint(admin));
+        System.out.println("totalsize for admin.delete ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin));
+
+
+        System.out.println("objectfootprintfor admin.undo ");
+        admin.undo();
+        logger.info(()->JvmUtilities.objectFootprint(admin));
+
+        System.out.println("totalsize for admin.undo ");
+        logger.info(()->JvmUtilities.objectTotalSize(admin));
+
+
+        System.out.println("jvm info");
+        logger.info(() -> JvmUtilities.jvmInfo());
+
+
+        System.out.println("objectfootprintfor member2.addme ");
+        member2.addme();
+        member2.unregisterme();
+        logger.info(()->JvmUtilities.objectFootprint(member2));
+
+        System.out.println("totalsize for member2.unregisterme ");
+        logger.info(()->JvmUtilities.objectTotalSize(member2));
+
+
+
+
+
     }
 }
