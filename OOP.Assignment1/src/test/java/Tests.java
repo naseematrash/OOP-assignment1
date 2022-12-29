d3b3528ed7388e68023a5b6095cb9438c4bb8ba4
naseem atrash <naseematrash12@gmail.com>
2022-12-29 18:05:24 +0200

    trying a Tests.java

--- OOP.Assignment1/src/test/java/Tests.java
+++ OOP.Assignment1/src/test/java/Tests.java
@@ -1,21 +1,27 @@
+import observer.ConcreteMember;
+import observer.GroupAdmin;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+
+import java.net.ConnectException;
 
 public class Tests {
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
     // stub method to check external dependencies compatibility
     @Test
     public void test(){
-        String s1 = "Alice";
-        String s2 = "Bob";
+        GroupAdmin admin =new GroupAdmin();
+        ConcreteMember member1 = new ConcreteMember(admin);
+        ConcreteMember member2 = new ConcreteMember(admin);
+        ConcreteMember member3 = new ConcreteMember(admin);
 
-        logger.info(()->JvmUtilities.objectFootprint(s1));
+       // logger.info(()->JvmUtilities.objectFootprint(s1));
 
-        logger.info(()->JvmUtilities.objectFootprint(s1,s2));
+        //logger.info(()->JvmUtilities.objectFootprint(s1,s2));
 
-        logger.info(()->JvmUtilities.objectTotalSize(s1));
+        //logger.info(()->JvmUtilities.objectTotalSize(s1));
 
-        logger.info(() -> JvmUtilities.jvmInfo());
+        //logger.info(() -> JvmUtilities.jvmInfo());
     }
 }
